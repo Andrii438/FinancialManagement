93e4691f6118a77637c85e912771238111ce06e8
Andrii <380679724615@mail.ru>
2024-03-14 13:51:09 +0100

    User service tests completed

--- src/test/java/com/example/financemanagement/service/UserServiceImplTest.java
+++ src/test/java/com/example/financemanagement/service/UserServiceImplTest.java
@@ -2,6 +2,7 @@ package com.example.financemanagement.service;
 
 import com.example.financemanagement.entity.User;
 import com.example.financemanagement.entity.builder.UserBuilder;
+import com.example.financemanagement.exception.UserNotFoundException;
 import com.example.financemanagement.repository.UserRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -11,12 +12,19 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.time.LocalDate;
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class UserServiceImplTest {
 
+    public static final long ID = 5L;
     @Mock
     private UserRepository userRepository;
 
@@ -32,6 +40,7 @@ class UserServiceImplTest {
     @BeforeEach
     void setUp() {
         user = new UserBuilder()
+                .id(ID)
                 .birthdate(LOCAL_DATE)
                 .firstName(FIRST_NAME)
                 .surname(SURNAME)
@@ -47,17 +56,46 @@ class UserServiceImplTest {
 
     @Test
     void deleteUserById() {
+        userService.deleteUserById(ID);
+
+        verify(userRepository).deleteById(ID);
     }
 
     @Test
     void createOrUpdateUser() {
+        userService.createOrUpdateUser(user);
+
+        verify(userRepository).save(user);
     }
 
     @Test
     void findAllUsers() {
+        when(userRepository.findAll()).thenReturn(Collections.singletonList(user));
+        List<User> users = userService.findAllUsers();
+
+        assertThat(users).hasSize(1);
+
+        verify(userRepository).findAll();
     }
 
     @Test
     void findUserById() {
+        when(userRepository.findById(ID)).thenReturn(Optional.of(user));
+        User foundUser = userService.findUserById(ID);
+
+        assertAll(
+                () -> assertThat(foundUser).isNotNull(),
+                () -> assertThat(foundUser.getUsername()).isEqualTo(USERNAME),
+                () -> assertThat(foundUser.getBirthdate()).isEqualTo(LOCAL_DATE),
+                () -> assertThat(foundUser.getFirstName()).isEqualTo(FIRST_NAME),
+                () -> assertThat(foundUser.getSurname()).isEqualTo(SURNAME)
+        );
+
+        verify(userRepository).findById(ID);
+    }
+
+    @Test()
+    void findUserByIdThrowsAnException() {
+        assertThrows(UserNotFoundException.class, () -> userService.findUserById(ID));
     }
 }
