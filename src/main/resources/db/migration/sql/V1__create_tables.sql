771e6df17be8c728e94c86f93075348cd45b6aaa
Andrii <380679724615@mail.ru>
2024-03-25 18:06:43 +0100

    Fix sonar issues

--- src/main/resources/db/migration/sql/V1__create_tables.sql
+++ src/main/resources/db/migration/sql/V1__create_tables.sql
@@ -1,14 +1,14 @@
 CREATE TABLE financial_user (
                         id SERIAL PRIMARY KEY,
-                        first_name VARCHAR(255),
-                        surname VARCHAR(255),
+                        first_name VARCHAR2(255),
+                        surname VARCHAR2(255),
                         birthdate DATE,
-                        username VARCHAR(255)
+                        username VARCHAR2(255)
 );
 
 CREATE TABLE financial_goal (
                                 id SERIAL PRIMARY KEY,
-                                goal_name VARCHAR(255),
+                                goal_name VARCHAR2(255),
                                 achieved_date DATE,
                                 amount DECIMAL(19, 2),
                                 user_id BIGINT,
